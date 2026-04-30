a42d0437a79fe23630da6f9379bf2208b7db113b
scx567888 <scx567888@outlook.com>
2026-04-30 16:34:44 +0800

    dev

---
+++ src/test/java/cool/scx/app/x/test/ExtModuleTest.java
@@ -0,0 +1,42 @@
+package cool.scx.app.x.test;
+
+
+import dev.scx.app.ScxApp;
+import dev.scx.app.ScxAppModule;
+import dev.scx.app.enumeration.ScxAppFeature;
+import dev.scx.app.x.crud.CRUDModule;
+import dev.scx.app.x.fix_table.FixTableModule;
+import dev.scx.app.x.fss.FSSModule;
+import dev.scx.app.x.redirect.RedirectModule;
+import dev.scx.app.x.static_server.StaticServerModule;
+import org.testng.annotations.Test;
+
+public class ExtModuleTest extends ScxAppModule {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    @Test
+    public static void test1() {
+        ScxApp.builder()
+                .setMainClass(ExtModuleTest.class)
+                .addModule(
+                        new ExtModuleTest(),
+                        new CRUDModule(),
+                        new FixTableModule(),
+                        new FSSModule(),
+                        new StaticServerModule(),
+                        new RedirectModule()
+                )
+                .configure(ScxAppFeature.USE_DEVELOPMENT_ERROR_PAGE, true)
+                .configure(ScxAppFeature.USE_SPY, true)
+                .run();
+    }
+
+    @Override
+    public void start(ScxApp scx) {
+        scx.fixTable();
+    }
+
+}
