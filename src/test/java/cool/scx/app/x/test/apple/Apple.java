908ba689500bd3d98446a7631da2dfaf6c1a922c
scx567888 <scx567888@outlook.com>
2026-04-30 16:34:35 +0800

    dev

---
+++ src/test/java/cool/scx/app/x/test/apple/Apple.java
@@ -0,0 +1,12 @@
+package cool.scx.app.x.test.apple;
+
+
+import dev.scx.app.base.BaseModel;
+import dev.scx.data.sql.annotation.Table;
+
+@Table("apple")
+public class Apple extends BaseModel {
+
+    public String name;
+
+}
---
+++ src/test/java/cool/scx/app/x/test/apple/AppleService.java
@@ -0,0 +1,9 @@
+package cool.scx.app.x.test.apple;
+
+import dev.scx.app.annotation.ScxService;
+import dev.scx.app.base.BaseModelService;
+
+@ScxService
+public class AppleService extends BaseModelService<Apple> {
+
+}
