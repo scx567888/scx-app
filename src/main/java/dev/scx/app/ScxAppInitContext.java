e32639d07d6a4b94a9194dc9c606fc57a8282382
scx567888 <scx567888@outlook.com>
2026-05-02 21:54:34 +0800

    dev

---
+++ src/main/java/dev/scx/app/ScxAppInitContext.java
@@ -0,0 +1,15 @@
+package dev.scx.app;
+
+import dev.scx.app._old.config.ScxConfig;
+import dev.scx.app._old.config.ScxEnvironment;
+
+// todo ?
+public interface ScxAppInitContext {
+
+    ScxConfig config();
+
+    ScxEnvironment environment();
+
+
+
+}
---
+++ src/main/java/dev/scx/app/ScxAppModule.java
@@ -0,0 +1,20 @@
+package dev.scx.app;
+
+import dev.scx.app._old.ScxApp;
+
+// todo ?
+public interface ScxAppModule {
+
+    default void init(ScxAppInitContext initContext) {
+
+    }
+
+    default void start(ScxApp scxApp) {
+
+    }
+
+    default void stop(ScxApp scxApp) {
+
+    }
+
+}
