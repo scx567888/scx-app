b38f0eb99699ac1b4265c827919e252a1f5e4581
scx567888 <scx567888@outlook.com>
2026-05-06 14:52:12 +0800

    dev

---
+++ src/main/java/dev/scx/app/config/ScxConfigSource.java
@@ -0,0 +1,26 @@
+package dev.scx.app.config;
+
+import dev.scx.node.ObjectNode;
+
+import java.util.function.BiConsumer;
+
+/// 配置源
+///
+/// @author scx567888
+/// @version 0.0.1
+public interface ScxConfigSource {
+
+    /// 获取当前配置值
+    ///
+    /// @return 当前配置值
+    ObjectNode value();
+
+    /// 监听配置值变化
+    ///
+    /// 如果当前配置源支持监听配置变化 (如: 文件).
+    ///
+    default void onChange(BiConsumer<ObjectNode, ObjectNode> changeListener) {
+
+    }
+
+}
