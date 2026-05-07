3334a393dd06f8bcb074bf23bf916abe2f1f4073
scx567888 <scx567888@outlook.com>
2026-05-07 18:05:50 +0800

    dev

--- src/main/java/dev/scx/app/TTT.java
+++ src/main/java/dev/scx/app/TTT.java
@@ -1,7 +1,13 @@
 package dev.scx.app;
 
+import dev.scx.app.component.ScxAppComponentModule;
 import dev.scx.app.cors.ScxAppCorsModule;
+import dev.scx.app.fix_table.FixTableModule;
+import dev.scx.app.fss.FSSModule;
 import dev.scx.app.http.ScxAppHttpModule;
+import dev.scx.app.logging.ScxAppLoggingModule;
+import dev.scx.app.scheduling.ScxAppSchedulingModule;
+import dev.scx.app.sql.ScxAppSQLModule;
 import dev.scx.app.static_server.ScxAppStaticServerModule;
 import dev.scx.app.web.ScxAppWebModule;
 
@@ -9,10 +15,16 @@ public class TTT {
 
     static void main(String[] args) {
         ScxApp.builder()
+            .module(new ScxAppLoggingModule())
+            .module(new ScxAppComponentModule())
             .module(new ScxAppHttpModule())
             .module(new ScxAppCorsModule())
             .module(new ScxAppWebModule())
             .module(new ScxAppStaticServerModule())
+            .module(new ScxAppSQLModule())
+            .module(new ScxAppSchedulingModule())
+            .module(new FixTableModule())
+            .module(new FSSModule())
             .build()
             .run();
     }
