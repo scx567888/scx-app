4193892fab8521a232cb113503fdbd43cd194dbe
scx567888 <scx567888@outlook.com>
2026-05-07 17:23:41 +0800

    dev

--- src/main/java/dev/scx/app/ScxAppModule.java
+++ src/main/java/dev/scx/app/ScxAppModule.java
@@ -1,21 +1,62 @@
 package dev.scx.app;
 
-
+/// ScxApp 的模块.
+///
+/// 一个 ScxAppModule 表示 ScxApp 启动流程中的一个动作单元.
+///
+/// ScxAppModule 的完整执行顺序如下:
+///
+/// 1. ScxApp 调用所有模块的 `init(ScxAppInitContext)`
+///    每个模块根据当前配置和初始化上下文创建自身所需的对象,
+///    并返回 ScxAppModuleDefinition.
+///
+/// 2. ScxApp 汇总所有 ScxAppModuleDefinition
+///    ScxApp 会根据所有模块返回的定义:
+///    - 收集组件候选类
+///    - 收集组件选择器
+///    - 收集组件实例
+///    - 构建并初始化 DI 容器
+///    - 根据 startBefore / startAfter 计算模块 start 顺序
+///
+/// 3. ScxApp 按计算出的顺序调用所有模块的 start(ScxApp)
+///    此时 DI 容器已经完成构建, 模块可以通过 ScxApp 访问应用运行时能力.
+///
+/// 4. ScxApp 在停止应用时, 按成功 start 的反向顺序调用 stop(ScxApp).
+///
+/// start 的含义是“执行该模块在启动流程中的动作”.
+/// 该动作可以是注册路由、修复数据库表、启动服务器、启动调度器等.
+///
+/// @author scx567888
+/// @version 0.0.1
 public interface ScxAppModule {
 
-    /// 收集 类, 注册 组件选择器
+    /// 初始化当前模块, 并返回当前模块在本次 ScxApp 中的定义.
     ///
-    /// @return
+    /// 该方法会在 DI 容器构建之前被调用.
+    /// ScxApp 会收集所有模块返回的 ScxAppModuleDefinition,
+    /// 并在之后统一构建 DI 容器和模块启动顺序.
+    ///
+    /// @param context 当前 ScxApp 的初始化上下文
+    /// @return 当前模块的定义信息
     default ScxAppModuleDefinition init(ScxAppInitContext context) {
         return ScxAppModuleDefinition.of();
     }
 
-    /// start
+    /// 执行当前模块的启动动作.
+    ///
+    /// 该方法会在所有模块 init 完成, 且 ScxApp 完成 DI 容器构建之后调用.
+    /// ScxApp 会根据各模块定义中的 startBefore / startAfter 计算调用顺序.
+    ///
+    /// @param scxApp 当前 ScxApp 实例
     default void start(ScxApp scxApp) {
 
     }
 
-    /// stop
+    /// 停止当前模块.
+    ///
+    /// ScxApp 会按成功 start 的反向顺序调用该方法.
+    ///
+    /// @param scxApp 当前 ScxApp 实例
     default void stop(ScxApp scxApp) {
 
     }
--- src/main/java/dev/scx/app/ScxAppModuleDefinition.java
+++ src/main/java/dev/scx/app/ScxAppModuleDefinition.java
@@ -1,14 +1,31 @@
 package dev.scx.app;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 
-/// ScxAppModule 的定义信息.
+/// ScxAppModule 在当前 ScxApp 中的定义信息.
 ///
-/// 模块通过此对象声明自己向 ScxApp 贡献的内容.
-/// 该对象只用于 ScxApp 构建阶段, 不表示运行时资源.
+/// 每个 ScxAppModule 会在 init(ScxAppInitContext) 阶段返回一个
+/// ScxAppModuleDefinition.
+///
+/// ScxApp 会在所有模块 init 完成后汇总这些定义, 并据此完成:
+///
+/// - 组件候选类收集
+/// - 组件选择器收集
+/// - 组件实例收集
+/// - DI 容器构建
+/// - 模块 start 顺序计算
+///
+/// startBefore / startAfter 同时表达模块存在关系和 start 顺序关系:
+///
+/// - startBefore(A) 表示当前模块的 start 在 A.start 之前执行,
+///   且 A 必须存在于当前 ScxApp 中.
+///
+/// - startAfter(A) 表示当前模块的 start 在 A.start 之后执行,
+///   且 A 必须存在于当前 ScxApp 中.
+///
+/// 如果 startBefore / startAfter 形成环, ScxApp 会在构建阶段失败.
 ///
 /// @author scx567888
 /// @version 0.0.1
@@ -36,46 +53,65 @@ public class ScxAppModuleDefinition {
         return new ScxAppModuleDefinition();
     }
 
+    /// 返回当前模块提供的组件候选类.
     public List<Class<?>> candidates() {
         return candidates;
     }
 
+    /// 添加当前模块提供的组件候选类.
     public ScxAppModuleDefinition candidate(Class<?>... candidates) {
         this.candidates.addAll(List.of(candidates));
         return this;
     }
 
+    /// 返回当前模块提供的组件选择器.
+    ///
+    /// ScxApp 会使用所有模块提供的组件选择器,
+    /// 从所有候选类中筛选出需要注册到 DI 容器的组件类.
     public List<Predicate<Class<?>>> componentSelectors() {
         return componentSelectors;
     }
 
+    /// 添加当前模块提供的组件选择器.
     public ScxAppModuleDefinition componentSelector(Predicate<Class<?>>... componentSelector) {
         this.componentSelectors.addAll(List.of(componentSelector));
         return this;
     }
 
+    /// 返回当前模块提供的组件实例.
+    ///
+    /// ScxApp 会将这些实例注册到 DI 容器.
     public List<Object> componentInstances() {
         return componentInstances;
     }
 
+    /// 添加当前模块提供的组件实例.
     public ScxAppModuleDefinition componentInstance(Object... component) {
         this.componentInstances.addAll(List.of(component));
         return this;
     }
 
+    /// 返回当前模块需要早于哪些模块执行 start.
     public List<Class<? extends ScxAppModule>> startBefores() {
         return startBefores;
     }
 
+    /// 声明当前模块的 start 需要早于指定模块执行.
+    ///
+    /// 被引用的模块必须存在于当前 ScxApp 中.
     public ScxAppModuleDefinition startBefore(Class<? extends ScxAppModule>... startBefores) {
         this.startBefores.addAll(List.of(startBefores));
         return this;
     }
 
+    /// 返回当前模块需要晚于哪些模块执行 start.
     public List<Class<? extends ScxAppModule>> startAfters() {
         return startAfters;
     }
 
+    /// 声明当前模块的 start 需要晚于指定模块执行.
+    ///
+    /// 被引用的模块必须存在于当前 ScxApp 中.
     public ScxAppModuleDefinition startAfter(Class<? extends ScxAppModule>... startAfters) {
         this.startAfters.addAll(List.of(startAfters));
         return this;
