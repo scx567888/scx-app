3f2d3b55109374454d807ebafa9f4b4d2939b960
scx567888 <scx567888@outlook.com>
2026-05-07 13:38:09 +0800

    dev

--- src/main/java/dev/scx/app/component/ScxAppComponentModule.java
+++ src/main/java/dev/scx/app/component/ScxAppComponentModule.java
@@ -1,4 +1,17 @@
 package dev.scx.app.component;
 
-public class ScxAppComponentModule {
+import dev.scx.app.ScxAppDefineContext;
+import dev.scx.app.ScxAppModule;
+import dev.scx.app.ScxAppModuleDefinition;
+
+public class ScxAppComponentModule implements ScxAppModule {
+
+    @Override
+    public ScxAppModuleDefinition define(ScxAppDefineContext context) {
+        return new ScxAppModuleDefinition()
+            .componentSelector(c ->
+                c.getAnnotation(Component.class) != null
+            );
+    }
+
 }
