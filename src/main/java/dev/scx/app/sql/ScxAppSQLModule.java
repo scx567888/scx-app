cf8928ab49f1838a082a5923ecf058fd96bd5e1b
scx567888 <scx567888@outlook.com>
2026-05-08 10:46:41 +0800

    dev

--- src/main/java/dev/scx/app/sql/ScxAppSQLModule.java
+++ src/main/java/dev/scx/app/sql/ScxAppSQLModule.java
@@ -1,6 +1,11 @@
 package dev.scx.app.sql;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import dev.scx.app.ScxAppInitContext;
 import dev.scx.app.ScxAppModule;
+import dev.scx.app.ScxAppModuleDefinition;
+import dev.scx.app.config.ScxConfig;
 import dev.scx.app.sql.handler.ObjectSQLHandlerFactory;
 import dev.scx.jdbc.spy.ScxJdbcSpy;
 import dev.scx.jdbc.spy.listener.logging.LoggingDataSourceListener;
@@ -11,20 +16,43 @@ import dev.scx.sql.TypeSQLResolver;
 
 public class ScxAppSQLModule implements ScxAppModule {
 
-    static SQLClient initSQLClient(ScxAppOptions scxOptions, ScxFeatureConfig scxFeatureConfig) {
+    @Override
+    public ScxAppModuleDefinition init(ScxAppInitContext context) {
+        SQLClient sqlClient = initSQLClient(context.config());
+
+        return ScxAppModuleDefinition.of().componentInstance(sqlClient);
+    }
+
+    static SQLClient initSQLClient(ScxConfig scxOptions) {
+        var dataSourceUrl=scxOptions.get("xxx", String.class);
+        var dataSourceUsername=scxOptions.get("xxx", String.class);
+        var dataSourcePassword=scxOptions.get("xxx", String.class);
+        var dataSourceParameters=scxOptions.get("xxx", String.class);
+
+        JDBCConnectionInfo jdbcConnectionInfo = new JDBCConnectionInfo(
+            dataSourceUrl,
+            dataSourceUsername,
+            dataSourcePassword,
+            dataSourceParameters
+        );
+
+        TypeSQLResolver build = TypeSQLResolver.builder()
+            .registerDefaultHandlers()
+            .registerHandlerFactory(new ObjectSQLHandlerFactory())
+            .build();
+
+        var useSpy= scxOptions.get("xxx", boolean.class);
 
         return SQLClient.of(
-            new JDBCConnectionInfo(
-                scxOptions.dataSourceUrl(),
-                scxOptions.dataSourceUsername(),
-                scxOptions.dataSourcePassword(),
-                scxOptions.dataSourceParameters()
-            ),
-            TypeSQLResolver.registerDefaultHandlers(TypeSQLResolver.builder())
-                .registerHandlerFactory(new ObjectSQLHandlerFactory())
-                .build(),
+            jdbcConnectionInfo,
+            build,
+            d->{
+                var hikariConfig = new HikariConfig();
+                hikariConfig.setDataSource(d);
+                return new HikariDataSource(hikariConfig);
+            },
             d-> {
-                return scxFeatureConfig.get(USE_SPY) ? ScxJdbcSpy.spy(d, new LoggingDataSourceListener(PreparedStatementLogStyle.RENDERED_SQL)): d;
+                return useSpy ? ScxJdbcSpy.spy(d, new LoggingDataSourceListener(PreparedStatementLogStyle.RENDERED_SQL)): d;
             }
         );
     }
